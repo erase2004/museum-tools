01fb774a97e45747827ce672c14fbd23fb9fb63d
Tsuki Akiba <erase2004@gmail.com>
2025-11-14 12:22:17 +0800

    refactor(037-add-company-founder): add round1 handle

--- src/mongo-scripts/037-add-company-founder.js
+++ src/mongo-scripts/037-add-company-founder.js
@@ -18,4 +18,42 @@ db.log.find({ logType: '創立公司' }).forEach(({ companyId, userId }) => {
     }
   });
 });
+
+// 針對第一季的處理，抓取創立得股的第一位使用者
+db.log.aggregate([
+  {
+    $match: {
+      logType: "創立得股"
+    }
+  },
+  {
+    $sort: {
+      createdAt: 1
+    }
+  },
+  {
+    $group: {
+      _id: "$companyId",
+      userId: {
+        $first: "$userId"
+      }
+    }
+  }
+]).forEach(({ _id: companyId, userId }) => {
+  const founder = userId[0];
+
+  bulkOperations.push({
+    updateOne: {
+      filter: {
+        _id: companyId
+      },
+      update: {
+        $set: {
+          founder
+        }
+      }
+    }
+  });
+});
+
 printjson(db.companies.bulkWrite(bulkOperations));
