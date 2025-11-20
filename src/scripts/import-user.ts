5563b5f6a7ece39b2053998ec9c2a666b0c1a318
Tsuki Akiba <erase2004@gmail.com>
2025-11-20 21:23:35 +0800

    feat(import-user): add own stock, product value and detailed favorites

--- src/scripts/import-user.ts
+++ src/scripts/import-user.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 import yargs from 'yargs';
 import { MongoClient } from 'mongodb';
-import { isUndefined, range } from 'lodash-es';
+import { entries, groupBy, isUndefined, keyBy, pick, range } from 'lodash-es';
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { DB_URI } from '@/configs/index';
@@ -109,20 +109,113 @@ const userSchema = z.object({
   profile: profileSchema,
 });
 
+const directorSchema = z
+  .object({
+    /** 公司 ID */
+    companyId: z.string(),
+    /** 董事 user ID */
+    userId: z.string(),
+    /** 擁有股份 */
+    stocks: integer.min(1),
+  }) // 縮短 key，減少輸出的檔案尺寸
+  .transform((value) => ({ u: value.userId, c: value.companyId, s: value.stocks }));
+
+const ownProductSchema = z.object({
+  /** 使用者 ID */
+  userId: z.string(),
+  /** 產品公司 ID */
+  companyId: z.string(),
+  /** 持有數量 */
+  amount: integer.min(1),
+  /** 產品價格 */
+  price: integer.min(1),
+});
+
+const companySchema = z.object({
+  _id: z.coerce.string(),
+  isSeal: z.boolean().default(false),
+  companyName: z.string().min(1).max(100),
+});
+
 async function main() {
   const client = new MongoClient(DB_URI);
   const firestore = getFirestore();
 
   async function generateTask(dbName: string, targetDbName: string) {
-    const dbUsers = client.db(dbName).collection('users');
-    const users = await z.promise(userSchema.array()).parseAsync(dbUsers.find({}).toArray());
-
-    console.debug(`[${dbName}] There are ${users.length} users`);
+    const connection = client.db(dbName);
 
     try {
+      // users
+      const dbUsers = connection.collection('users');
+      const users = await z.promise(userSchema.array()).parseAsync(dbUsers.find({}).toArray());
+
+      // user own stocks
+      const dbDirectors = connection.collection('directors');
+      const ownStockList =
+        (await z.promise(directorSchema.array()).parseAsync(dbDirectors.find({}).toArray())) ?? [];
+      const fullMap = groupBy(ownStockList, 'u');
+      const ownStockMap = entries(fullMap).reduce(
+        (m, [userId, values]) => {
+          m[userId] = values.map((v) => pick(v, ['c', 's']));
+          return m;
+        },
+        {} as Record<string, { c: string; s: number }[]>,
+      );
+
+      // user own product values
+      const dbSeason = connection.collection('season');
+      const lastSeason = await z
+        .promise(z.object({ _id: z.coerce.string() }))
+        .parseAsync(dbSeason.findOne({}, { sort: { beginDate: -1 } }));
+      const dbUserOwnedProduct = connection.collection('userOwnedProducts');
+      const list =
+        (await z
+          .promise(ownProductSchema.array())
+          .parseAsync(dbUserOwnedProduct.find({ seasonId: lastSeason._id }).toArray())) ?? [];
+      const dict = groupBy(list, 'userId');
+      const ownProductValueMap = entries(dict).reduce(
+        (userMap, [userId, items]) => {
+          userMap[userId] = items.reduce(
+            (totalMap, item) => {
+              const companyId = item.companyId;
+
+              totalMap[companyId] = (totalMap[companyId] || 0) + item.price * item.amount;
+              return totalMap;
+            },
+            {} as Record<string, number>,
+          );
+
+          return userMap;
+        },
+        {} as Record<string, Record<string, number>>,
+      );
+
+      // company
+      const dbCompanies = connection.collection('companies');
+      const companyList =
+        (await z.promise(companySchema.array()).parseAsync(dbCompanies.find({}).toArray())) ?? [];
+      const companyMap = keyBy(companyList, '_id');
+
+      console.debug(`[${dbName}] There are ${users.length} users`);
+
       const collectionRef = firestore.collection(targetDbName);
       for (const user of users) {
-        await collectionRef.add(user);
+        const id = user._id;
+        const existedItem = await collectionRef.where('_id', '==', id).get();
+
+        const data = {
+          ...user,
+          ownStock: ownStockMap[id] ?? {},
+          productValue: ownProductValueMap[id] ?? {},
+          favorite: [],
+          favoriteV2: user.favorite.map((id) => companyMap[id]),
+        };
+
+        if (existedItem.empty) {
+          await collectionRef.add(data);
+        } else {
+          await collectionRef.doc(existedItem.docs[0]!.id).update(data);
+        }
       }
 
       console.debug(`[${dbName}] Finished adding users`);
